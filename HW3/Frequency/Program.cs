e85f4bebeef3f37c1bb503533ca171ae1949c99e
eulalioshaira <eulalioshaira@gmail.com>
2025-09-30 21:36:23 +0800

    frequency

---
+++ HW3/Frequency/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharFrequency
+{
+    class Program
+    {
+        static Dictionary<char, int> GetFrequency(string input)
+        {
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+
+            foreach (char c in input.ToLower()) 
+            {
+                if (c == ' ' || c == ',')
+                    continue;
+
+                if (freq.ContainsKey(c))
+                    freq[c]++;
+                else
+                    freq[c] = 1;
+            }
+
+            return freq;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 1 or more strings separated by comma: ");
+            string input = Console.ReadLine();
+            string[] words = input.Split(',');
+
+            foreach (string word in words)
+            {
+                string trimmedWord = word.Trim(); 
+                Dictionary<char, int> frequency = GetFrequency(trimmedWord);
+
+                List<string> parts = new List<string>();
+                foreach (var kvp in frequency)
+                {
+                    parts.Add($"{kvp.Key}={kvp.Value}");
+                }
+
+                Console.WriteLine(string.Join(", ", parts));
+            }
+            Console.ReadKey();
+        }
+    }
+}
